b8c14223df3e9fa3a684a0c3efd4a2f774dfea35
Volkov <vad.volkov91@gmail.com>
2024-02-16 23:25:21 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.function.*;
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1,1);
+        int c = calc.multiply.apply(4, b);
+        int d = calc.devide.apply(a, b); // на ноль делить нельзя
+
+        calc.println.accept(d);
+    }
+
+}
